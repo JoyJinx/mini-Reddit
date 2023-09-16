84c4a48ad083c496c96e323af70521bfb625dd03
Farhad <120501202+JoyJinx@users.noreply.github.com>
2023-09-16 15:45:39 +0330

    added base

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
+const engine = require("ejs-mate");
+const methodOverride = require("method-override");
+
+app.engine("ejs", engine);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/public", express.static(path.join(__dirname, "public")));
+app.use(
+  "/css",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))
+);
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/js"))
+);
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "node_modules/jquery/dist"))
+);
+app.use(
+  "/fa",
+  express.static(path.join(__dirname, "node_modules/font-awesome/css"))
+);
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
 app.listen(3000, () => {
   console.log("Listening on port 3000:");
