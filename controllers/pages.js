be34ee85f5799a54298f8236e5ee45d6fea8e18f
Farhad <120501202+JoyJinx@users.noreply.github.com>
2023-10-02 12:33:19 +0330

    time posted updated  added number of comments

--- controllers/pages.js
+++ controllers/pages.js
@@ -2,7 +2,26 @@ const Page = require("../models/pages");
 const cloudinary = require("../cloudinary/index");
 
 module.exports.getPages = async (req, res) => {
-  const pages = await Page.find({}).populate("author");
+  let pages = await Page.find({}).populate("author");
+  for (let page of pages) {
+    const timePosted = Date.now() - page.date;
+
+    if (timePosted < 60 * 60 * 1000) {
+      page.humanDate = `${Math.floor(timePosted / (60 * 1000))} mins ago.`;
+    } else if (timePosted < 24 * 60 * 60 * 1000) {
+      page.humanDate = `${Math.floor(
+        timePosted / (60 * 60 * 1000)
+      )} hours ago.`;
+    } else {
+      page.humanDate = `${Math.floor(
+        timePosted / (24 * 60 * 60 * 1000)
+      )} days ago.`;
+    }
+  }
+  function compareNewFn(a, b) {
+    return b.date - a.date;
+  }
+  pages = pages.sort(compareNewFn);
   res.render("pages/index.ejs", { pages });
 };
 
--- controllers/sort.js
+++ controllers/sort.js
@@ -2,6 +2,21 @@ const Page = require("../models/pages");
 
 module.exports.getByNew = async (req, res) => {
   let pages = await Page.find({}).populate("author");
+  for (let page of pages) {
+    const timePosted = Date.now() - page.date;
+
+    if (timePosted < 60 * 60 * 1000) {
+      page.humanDate = `${Math.floor(timePosted / (60 * 1000))} mins ago.`;
+    } else if (timePosted < 24 * 60 * 60 * 1000) {
+      page.humanDate = `${Math.floor(
+        timePosted / (60 * 60 * 1000)
+      )} hours ago.`;
+    } else {
+      page.humanDate = `${Math.floor(
+        timePosted / (24 * 60 * 60 * 1000)
+      )} days ago.`;
+    }
+  }
   function compareNewFn(a, b) {
     return b.date - a.date;
   }
@@ -11,6 +26,21 @@ module.exports.getByNew = async (req, res) => {
 
 module.exports.getByBest = async (req, res) => {
   let pages = await Page.find({}).populate("author");
+  for (let page of pages) {
+    const timePosted = Date.now() - page.date;
+
+    if (timePosted < 60 * 60 * 1000) {
+      page.humanDate = `${Math.floor(timePosted / (60 * 1000))} mins ago.`;
+    } else if (timePosted < 24 * 60 * 60 * 1000) {
+      page.humanDate = `${Math.floor(
+        timePosted / (60 * 60 * 1000)
+      )} hours ago.`;
+    } else {
+      page.humanDate = `${Math.floor(
+        timePosted / (24 * 60 * 60 * 1000)
+      )} days ago.`;
+    }
+  }
   function compareBestFn(a, b) {
     return b.likes.length - a.likes.length;
   }
