29232835764bb69f01052c1ca3d137149d3ad5ce
Farhad <120501202+JoyJinx@users.noreply.github.com>
2023-10-03 16:27:46 +0330

    starting to add user overview

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,11 +1,16 @@
 const Comment = require("../models/comments");
 const Page = require("../models/pages");
+const User = require("../models/users");
 
 module.exports.postComment = async (req, res) => {
   const subpage = await Page.findById(req.params.id);
   const comment = new Comment(req.body.comment);
+  const thisUser = User.findById(req.user._id);
   comment.author = req.user._id;
   subpage.comments.unshift(comment);
+  thisUser.comments.unshift(comment);
+
+  await thisUser.save();
   await comment.save();
   await subpage.save();
   res.redirect(`/p/${subpage._id}`);
@@ -14,16 +19,20 @@ module.exports.postComment = async (req, res) => {
 module.exports.postCommentLike = async (req, res) => {
   const { id, commentId } = req.params;
   const foundComment = await Comment.findById(commentId);
+  const thisUser = await User.findById(req.user._id);
   const foundUserCommentLike = foundComment.likes.some(function (like) {
     return like.equals(req.user._id);
   });
   if (foundUserCommentLike) {
     foundComment.likes.pull(req.user._id);
+    thisUser.likes.pull(commentId);
     req.flash("success", "removed like!!");
   } else {
     foundComment.likes.unshift(req.user._id);
+    thisUser.like.unshift(commentId);
     req.flash("success", "liked!");
   }
+  await thisUser.save();
   await foundComment.save();
   res.redirect(`/p/${id}`);
 };
@@ -44,6 +53,9 @@ module.exports.patchComment = async (req, res) => {
 module.exports.commentDelete = async (req, res) => {
   const { id, commentId } = req.params;
   await Page.findByIdAndUpdate(id, { $pull: { comments: commentId } });
+  await User.findByIdAndUpdate(req.user._id, {
+    $pull: { comments: commentId, likes: commentId },
+  });
   await Comment.findByIdAndDelete(commentId);
   res.redirect(`/p/${id}`);
 };
--- models/users.js
+++ models/users.js
@@ -22,11 +22,13 @@ const userSchema = new mongoose.Schema({
   likes: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "Like",
+      ref: ["Page", "Comment"],
     },
   ],
 });
 
 userSchema.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
